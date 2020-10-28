4e0be015f0e84e869826684172dec91f06206d1c
Carlos Garcia <carlosgarcia@stratio.com>
2020-10-28 13:25:29 +0100

    Add VaultAnsible decrypt

--- src/main/java/com/stratio/qa/utils/vaultansible/VaultAnsible.java
+++ src/main/java/com/stratio/qa/utils/vaultansible/VaultAnsible.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.stratio.qa.utils.vaultansible;
 
 import org.apache.commons.io.IOUtils;
